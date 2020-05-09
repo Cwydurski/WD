ffb0a81528c33fc8465a9be682d783aa3c56bad6
Cezary Wydurski <czarekwydurski123@wp.pl>
2020-05-09 19:02:28 +0200

    zad2

---
+++ zad2.py
@@ -0,0 +1,9 @@
+import sys
+
+wartosc1 = sys.stdin.readline()
+wartosc2 = sys.stdin.readline()
+wartosc1 = int(wartosc1)
+wartosc2 = int(wartosc2)
+s = wartosc1 * wartosc2
+s = str(s)
+sys.stdout.write(s)
